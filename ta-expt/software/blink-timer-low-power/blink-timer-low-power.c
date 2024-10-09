e0e2e1a1d9323f00e2574da1a5067931ad94c733
andrewmcg1 <greekgeek191@gmail.com>
2024-10-09 07:30:49 -0400

    updated low power blink

--- ta-expt/software/blink-timer-low-power/blink-timer-low-power.c
+++ ta-expt/software/blink-timer-low-power/blink-timer-low-power.c
@@ -15,29 +15,31 @@
 #include <libopencm3/cm3/scb.h>
 
 int main(void) {
+  /** Setup GPIO pins to control LEDs */
   rcc_periph_clock_enable(RCC_GPIOC);
-  rcc_periph_clock_enable(RCC_TIM2);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10);
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
   gpio_set(GPIOC, GPIO10);
   gpio_clear(GPIOC, GPIO12);
 
-  scb_clear_sleepdeep();
-  scb_set_sleeponexit();
-
+  /** Setup the timer to call the interrupt */
+  rcc_periph_clock_enable(RCC_TIM2);
   timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT_MUL_2, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
   timer_set_period(TIM2, 1250000);
   timer_enable_irq(TIM2, TIM_DIER_UIE);
   nvic_enable_irq(NVIC_TIM2_IRQ);
   timer_enable_counter(TIM2);
 
-  while(1) {
-    __asm__("nop");
-  }
+  /** Enable sleep mode */
+  scb_clear_sleepdeep();        // Clear SLEEPDEEP bit to make sure the MCU goes into the correct power mode
+  scb_set_sleeponexit();        // Enable sleep when exiting from an interrupt
+
+  __asm__("wfi");               // Wait for the first interrupt
 }
 
+/** Timer Interrupt */
 void tim2_isr(void) {
-  timer_clear_flag(TIM2, TIM_SR_UIF);
-  gpio_toggle(GPIOC, GPIO10);
-  gpio_toggle(GPIOC, GPIO12);
+  timer_clear_flag(TIM2, TIM_SR_UIF);   // clear flag so timer will continue counting
+  gpio_toggle(GPIOC, GPIO10);           // blinks LED
+  gpio_toggle(GPIOC, GPIO12);           // blinks LED
 }
